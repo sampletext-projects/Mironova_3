0c609671b1fc2ed4212a53db4a731c83386213ea
Bird Egop <sampletext32@bk.ru>
2021-04-02 17:53:57 +0300

    Add project

---
+++ Mironova_3/Mironova_3/Mironova_3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+
+using namespace std;
+
+double findSquare(double x, double  y, double z)
+{
+	double p = (x + y + z) / 2;
+	double square = sqrt(p * (p - x) * (p - y) * (p - z));
+	return square;
+}
+
+int main()
+{
+	setlocale(0, "Russian");
+	double a, b, c, d, e, f, g;
+	cout << "Введите a,b,c,d,e,f,g соответственно:" << endl;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	cin >> e;
+	cin >> f;
+	cin >> g;
+
+	double square = findSquare(a, b, f) + findSquare(f, g, c) + findSquare(d, e, g);
+	cout << square;
+}
+
